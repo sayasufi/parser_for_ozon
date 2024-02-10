82ec4e257a1d912ad8777ce40c1a3b5b43b5ed51
semyon <semen.ryaboff@yandex.ru>
2024-02-10 15:42:24 +0300

    Init

---
+++ source/get_cookies.py
@@ -0,0 +1,15 @@
+import os
+import pickle
+import time
+
+from selenium import webdriver
+
+browser = webdriver.Chrome()
+browser.get('https://www.ozon.ru/')
+
+if not os.path.exists("session"):
+    time.sleep(10)
+    pickle.dump(browser.get_cookies(), open("session", "wb"))
+else:
+    for cookie in pickle.load(open("session", "rb")):
+        browser.add_cookie(cookie)
